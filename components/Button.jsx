74f6cdec3b72bcb98a48852d6aaa6941340df862
TamimOp <tamimshadanik@gmail.com>
2025-05-10 20:43:18 +0600

    customised gradient color added and button edited according to the theme

--- components/Button.jsx
+++ components/Button.jsx
@@ -12,10 +12,10 @@ const Button = ({
       className={`group relative flex items-center gap-3 px-6 py-3 rounded-full transition-all duration-500 ${className}`}
       onClick={onClick}
     >
-      <div className="group-hover:opacity-0 transition-all duration-300 bg-[#06197D] rounded-full p-2 flex items-center justify-center">
+      <div className="group-hover:opacity-0 transition-all duration-300 bg-gradient-blue rounded-full p-2 flex items-center justify-center">
         <Image src={iconSrc} width={19} height={19} alt="icon" />
       </div>
-      <p className="text-[#06197D] text-xl font-semibold transition-all duration-500 group-hover:text-white group-hover:translate-x-[-50px]">
+      <p className="gradient-text text-xl font-semibold transition-all duration-500 group-hover:text-white group-hover:translate-x-[-50px]">
         {label}
       </p>
       <div className="absolute right-6 opacity-0 group-hover:opacity-100 translate-x-4 group-hover:translate-x-0 transition-all duration-500 border border-white rounded-full p-2">
