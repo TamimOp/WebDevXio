ef26d89fe7c7461f945b1f91f03a5f1cc527e020
TamimOp <tamimshadanik@gmail.com>
2025-05-12 03:52:32 +0600

    Fix positioning in Circle component: update top value for better layout

--- components/Circle.jsx
+++ components/Circle.jsx
@@ -114,7 +114,7 @@ export default function Circle() {
   return (
     <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden my-20">
       {/* Arc Container */}
-      <div className="absolute inset-0 flex items-center justify-center sm:top-210 sm:mt-10">
+      <div className="absolute inset-0 flex items-center justify-center top-25 sm:top-210 sm:mt-10">
         <div className="relative w-[1500px] h-[900px]">
           <svg
             width="100%"
