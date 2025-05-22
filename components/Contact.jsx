9d53926d86295cb2e6714facfbb56ed8a1a68836
TamimOp <tamimshadanik@gmail.com>
2025-05-22 20:19:52 +0600

    enhance Contact component layout and styling; add background image

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,12 +10,12 @@ import {
 const Contact = () => {
   const Button = ({ label, Icon, onClick }) => {
     return (
-      <div className="w-[200px] h-[55px] bg-gradient-to-r from-[#0634FF] to-[#B2ACFF] shadow-lg shadow-[#B2ACFF] flex items-center justify-center rounded-4xl">
+      <div className="w-[215px] h-[55px] bg-gradient-to-r from-[#0634FF] to-[#B2ACFF] shadow-lg shadow-[#B2ACFF] flex items-center justify-center rounded-4xl">
         <button
           onClick={onClick}
           className="relative overflow-hidden bg-gradient-to-r cursor-pointer 
-          flex group justify-center items-center gap-3 text-[22px] font-semibold text-white 
-          w-[195px] h-[50px] from-[#06197d] to-[#274afd] px-4 rounded-4xl 
+          flex group justify-center items-center gap-2 text-xl font-medium text-white 
+          w-[210px] h-[50px] from-[#06197d] to-[#274afd] px-4 rounded-4xl 
           hover:from-black hover:to-gray-500 hover:flex-row-reverse 
           transition-all duration-500 ease-in-out"
         >
@@ -35,24 +35,28 @@ const Contact = () => {
   };
   return (
     <section className="w-full bg-[#F7F8FC] py-16 px-6 md:px-12 lg:px-20">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12">
+      <div className="flex justify-around">
         {/* Left Side */}
         <div
-          className="rounded-3xl p-8 text-white flex flex-col justify-between"
+          className="w-[453px] h-[632px] rounded-3xl p-10 text-white flex flex-col justify-between"
           style={{
             background: "linear-gradient(180deg, #132663 0%, #000F3F 100%)",
           }}
         >
           <div>
-            <h3 className="text-2xl font-semibold mb-4">Address</h3>
-            <p className="mb-6">
+            <h3 className="text-[28px] font-bold mb-4">Address</h3>
+            <p className="text-[15px] font-medium text-[#A9ADB5] mb-6">
               3400 Cottage Way, Suite G2 #26176
               <br />
               Sacramento, CA 95825, USA
             </p>
-            <h3 className="text-2xl font-semibold mb-4">Contact</h3>
-            <p className="mb-1">Phone: +(000)000000000</p>
-            <p>Email: contact@webdevxio.com</p>
+            <h3 className="text-[28px] font-bold mb-4">Contact</h3>
+            <p className="text-[15px] font-medium text-[#A9ADB5] mb-1">
+              Phone: +(000)000000000
+            </p>
+            <p className="text-[15px] font-medium text-[#A9ADB5]">
+              Email: contact@webdevxio.com
+            </p>
           </div>
 
           <div className="relative mt-10">
@@ -63,8 +67,7 @@ const Contact = () => {
               className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-20"
             />
             <div className="relative border border-white rounded-xl p-6 backdrop-blur-sm">
-              <div className="text-4xl text-[#3B82F6] mb-4">“</div>
-              <p>
+              <p className="text-[16px] font-semibold">
                 Let’s build something amazing together! 🚀 Whether you need a
                 stunning SaaS website or a complete redesign, we’re here to
                 help. Drop us a message.
@@ -73,7 +76,7 @@ const Contact = () => {
           </div>
 
           <div className="mt-10">
-            <h3 className="text-lg font-medium mb-4">Stay Connected</h3>
+            <h3 className="text-[22px] font-medium mb-4">Stay Connected</h3>
             <div className="flex gap-4 text-xl">
               <FaFacebookF className="hover:text-blue-500 cursor-pointer" />
               <FaInstagram className="hover:text-pink-500 cursor-pointer" />
@@ -84,11 +87,11 @@ const Contact = () => {
         </div>
 
         {/* Right Side */}
-        <div>
-          <h4 className="text-sm font-semibold text-blue-600 mb-2">
+        <div className="flex flex-col gap-5 w-[696px] h-[748px] ">
+          <h4 className="text-[22px] font-medium text-blue-600 mb-2">
             —Contact Us
           </h4>
-          <h2 className="text-3xl md:text-4xl font-bold mb-8">
+          <h2 className="text-4xl md:text-5xl font-medium mb-8">
             Get Your <span className="text-blue-600">Free Quote</span> Today!
           </h2>
           <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -128,7 +131,7 @@ const Contact = () => {
             <div className="md:col-span-2">
               <label className="block mb-1 font-medium">Message</label>
               <textarea
-                rows="4"
+                rows="5"
                 placeholder="Enter here.."
                 className="w-full p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#F1F3FF]"
               ></textarea>
