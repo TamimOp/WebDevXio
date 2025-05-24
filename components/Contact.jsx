2b7f837a82fd220902d9c230c4c17127d0bf0f2c
TamimOp <tamimshadanik@gmail.com>
2025-05-24 14:32:58 +0600

    refactor Contact and Hero components for improved layout and responsiveness

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
 
   return (
     <section className="w-full bg-[#F7F8FC] py-16 px-6 md:px-12 lg:px-20">
-      <div className="flex flex-col lg:flex-row justify-between gap-10">
+      <div className="flex flex-col lg:flex-row justify-center gap-20">
         {/* Left Side */}
         <motion.div
           initial={{ opacity: 0, x: -50 }}
@@ -101,7 +101,7 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="mt-10">
+          <div className="mt-10 z-10">
             <h3 className="text-[20px] md:text-[22px] font-medium mb-4">
               Stay Connected
             </h3>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,8 @@ import Company from "./Company";
 
 export default function HeroNew() {
   return (
-    <section className="w-full flex flex-col gap-10">
-      <div className="hero">
+    <section className="w-full flex flex-col gap-2">
+      <div className="hero h-[70vh] sm:h-[100vh]">
         <Circle />
       </div>
       <Company />
