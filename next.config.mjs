eb1f433c80d600daf647f0e3aa173aa64de50193
TamimOp <tamimshadanik@gmail.com>
2025-07-27 17:37:03 +0600

    Add caching headers to improve performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2592000, immutable", // 30 days in seconds
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
