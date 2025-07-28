5233676db8b5cc9f696763563a1cc18bdc4c12f1
TamimOp <tamimshadanik@gmail.com>
2025-07-28 21:24:10 +0600

    Remove caching headers configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/_next/static/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=2592000, immutable", // 30 days in seconds
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
