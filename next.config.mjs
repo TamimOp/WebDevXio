441c6fde092b16e8b1d21b8b8410bfb15fb91049
TamimOp <tamimshadanik@gmail.com>
2025-07-28 21:52:22 +0600

    Refactor next.config.mjs to use ES module syntax for image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 // next.config.js
-module.exports = {
-  images: {
-    formats: [
-      "image/webp",
-      "image/avif",
-      "image/png",
-      "image/jpeg",
-      "image/svg",
-    ],
-    domains: ["webdevxio.com"], // if images are hosted externally
-  },
+export const images = {
+  formats: ["image/webp", "image/avif", "image/png", "image/jpeg", "image/svg"],
+  domains: ["webdevxio.com"], // if images are hosted externally
 };
